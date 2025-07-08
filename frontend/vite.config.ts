8966f9b5ec7a41b06d4327ff64be583ccf3e8604
rishi <rishidevtech09@gmail.com>
2025-07-08 17:10:30 +0530

    fixed build issue

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,7 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
 export default defineConfig({
   plugins: [react()],
-})
+  preview: {
+ 
+    allowedHosts: ['expense-tracker-1-0iev.onrender.com'],
+  },
+});
